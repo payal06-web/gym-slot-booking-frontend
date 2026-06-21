42b3b1cfcdbef1bb52ebf8506122aa93531791e1
Payal <pihuuprajapat4444@gmail.com>
2026-06-21 16:17:55 +0530

    add

--- src/api.js
+++ src/api.js
@@ -5,12 +5,22 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const user = JSON.parse(localStorage.getItem("user"));
-
-  if (user?.token) {
-    config.headers.Authorization = `Bearer ${user.token}`;
+  const token = localStorage.getItem("token");
+  console.log("Token: ", token)
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
+
+// api.interceptors.request.use((config) => {
+//   const user = JSON.parse(localStorage.getItem("user"));
+
+//   if (user?.token) {
+//     config.headers.Authorization = `Bearer ${user.token}`;
+//   }
+//   return config;
+// });
 
 export default api;
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,7 +5,7 @@ export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(()=>{
     try {
-    JSON.parse(localStorage.getItem("user"))
+    return JSON.parse(localStorage.getItem("user"))
 }catch(err){
     return null;
 }}
