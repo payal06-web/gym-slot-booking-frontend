e6773e93c2282501ea92d70fe4180e7d70e86a60
Payal <pihuuprajapat4444@gmail.com>
2026-06-21 16:27:07 +0530

    add

--- src/api.js
+++ src/api.js
@@ -5,22 +5,12 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  console.log("Token: ", token)
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+  const user = JSON.parse(localStorage.getItem("user"));
 
+  if (user?.token) {
+    config.headers.Authorization = `Bearer ${user.token}`;
+  }
   return config;
 });
-
-// api.interceptors.request.use((config) => {
-//   const user = JSON.parse(localStorage.getItem("user"));
-
-//   if (user?.token) {
-//     config.headers.Authorization = `Bearer ${user.token}`;
-//   }
-//   return config;
-// });
 
 export default api;
--- src/pages/AboutGym.jsx
+++ src/pages/AboutGym.jsx
@@ -6,14 +6,6 @@ const AboutGym = () => {
   const videos = [
     "/video1.mp4",
     "/video2.mp4",
-    "/video3.mp4",
-    "/video4.mp4",
-    "/video5.mp4",
-    "/video6.mp4",
-    "/video7.mp4",
-    "/video8.mp4",
-    "/video9.mp4",
-    "/video10.mp4",
   ];
 
   const images = [
